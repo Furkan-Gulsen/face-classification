e226d54e7af33a31e757d764f06700373dbae0ba
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-02-28 00:56:30 +0300

    detectFacesWithDNN

--- faceDetection/faceDetectionMethods.py
+++ faceDetection/faceDetectionMethods.py
@@ -1,15 +1,42 @@
 # Importing required packages
 import numpy as np
+import dlib
 import cv2
 
 
-def detectFacesWithDNN(frame):
-    # pre-trained model
-    modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel" 
-    # prototxt has the information of where the training data is located.
-    configFile = "models/deploy.prototxt" 
-    net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+def putText(frame, text, x, y, h):
+    color = (108, 72, 200) # It is the color of text string to be drawn
+    font = cv2.FONT_HERSHEY_SIMPLEX # It denotes the font type
+    thickness = 2 # It is the thickness of the line in px
+    fontScale = 1 # Font scale factor that is multiplied by the font-specific base size
+    org = (x+5, y+h-5) # It is the coordinates of the bottom-left corner of the text string in the image
+    lineType = cv2.LINE_AA # It gives the type of the line to be used
+    cv2.putText(frame, text, org, font, fontScale , color, thickness, lineType)
+    return frame
 
+
+def shapePoints(shape):
+    coords = np.zeros((68, 2), dtype="int")
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
+
+
+def rectPoints(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
+
+
+# pre-trained model
+modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel" 
+# prototxt has the information of where the training data is located.
+configFile = "models/deploy.prototxt" 
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+
+def detectFacesWithDNN(frame):
     # A neural network that really supports the input value
     size = (300,300)
 
@@ -31,5 +58,7 @@ def detectFacesWithDNN(frame):
         if confidence > 0.5:
             box = dnnFaces[0, 0, i, 3:7] * np.array([width, height, width, height])
             (x, y, x1, y1) = box.astype("int")
-            cv2.rectangle(frame, (x, y), (x1, y1),  (193, 69, 42), 2)
+            cv2.rectangle(frame, (x, y), (x1, y1), (193, 69, 42), 2)
+            # frame = putText(frame, "DNN", x+5, y+x1-5)
     return frame
+
