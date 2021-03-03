edb5357dcd90e39402bf31a5007d1457fa56ac63
furkan.gulsen <m.furkangulsen@hotmail.com>
2021-03-04 02:17:09 +0300

    gender classification

--- genderClassification.py
+++ genderClassification.py
@@ -1,9 +1,13 @@
 # Importing required packages
 from keras.models import load_model
 import numpy as np
+import argparse
 import dlib
 import cv2
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-vw", "--isVideoWriter", type=bool, default=False)
+args = vars(ap.parse_args())
 
 def shapePoints(shape):
     coords = np.zeros((68, 2), dtype="int")
@@ -20,7 +24,7 @@ def rectPoints(rect):
     return (x, y, w, h)
 
 
-faceLandmarks = "shape_predictor_68_face_landmarks.dat"
+faceLandmarks = "faceDetection/models/dlib/shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(faceLandmarks)
 
@@ -28,8 +32,15 @@ genderModelPath = 'models/genderModel.hdf5'
 genderClassifier = load_model(genderModelPath, compile=False)
 genderTargetSize = genderClassifier.input_shape[1:3]
 
+label = {0:"Female",1:"Male"} 
+
 cap = cv2.VideoCapture(0)
-label = {0:"female",1:"male"} 
+
+if args["isVideoWriter"] == True:
+    fourrcc = cv2.VideoWriter_fourcc("M","J","P","G")
+    capWidth = int(cap.get(3))
+    capHeight = int(cap.get(4))
+    videoWrite = cv2.VideoWriter("output.avi", fourrcc, 22, (capWidth, capHeight))
 
 while True:
     ret, frame = cap.read()
@@ -37,28 +48,34 @@ while True:
     if not ret:
         break
 
-
     rects = detector(frame, 0)
     for rect in rects:
-    	shape = predictor(frame, rect)
-    	points = shapePoints(shape)
-    	(x, y, w, h) = rectPoints(rect)
-    	resized = frame[y: y+h, x:x+w]
-    	try:
-    		frame_resize = cv2.resize(resized, genderTargetSize)
-    	except:
-    		continue
-    	frame_resize = frame_resize.astype('float32')
-    	frame_scaled = frame_resize/255.0
-    	reshape = np.reshape(frame_scaled,(1, 150, 150 ,3))
-    	img = np.vstack([reshape])
-    	gender_prediction = emotionClassifier.predict(img)
-    	gender_probability = np.max(gender_prediction)
-    	gender_label = np.argmax(gender_prediction)
-    	cv2.putText(frame, label[gender_label] + "("+ str(gender_probability) + ")" , (x+5, y+h-5),
-    		cv2.FONT_HERSHEY_SIMPLEX, 1 , (255,255,255), 1, cv2.LINE_AA)
-    	cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255,255), 2)
-    
+        shape = predictor(frame, rect)
+        points = shapePoints(shape)
+        (x, y, w, h) = rectPoints(rect)
+        resized = frame[y: y+h, x:x+w]
+        try:
+            frame_resize = cv2.resize(resized, genderTargetSize)
+        except:
+            continue
+
+        frame_resize = frame_resize.astype('float32')
+        frame_scaled = frame_resize/255.0
+        frame_reshape = np.reshape(frame_scaled,(1, 150, 150 ,3))
+        frame_vstack = np.vstack([frame_reshape])
+        gender_prediction = genderClassifier.predict(frame_vstack)
+        gender_probability = np.max(gender_prediction)
+        if(gender_probability > 0.5):
+            gender_result = label[np.argmax(gender_prediction)]
+            cv2.putText(frame, gender_result , (x+5, y+h-5),
+                cv2.FONT_HERSHEY_SIMPLEX, 1 , (218, 229, 97), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (218, 229, 97), 2)
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+    if args["isVideoWriter"] == True:
+        videoWrite.write(frame)
+
     cv2.imshow("Emotion Recognition", frame)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
@@ -66,4 +83,6 @@ while True:
 
 
 cap.release()
+if args["isVideoWriter"] == True:
+    videoWrite.release()
 cv2.destroyAllWindows()
