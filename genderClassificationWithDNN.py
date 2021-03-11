307cb93fa5cb4357ddeccf81205c5a1f5d9ae25c
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-03-11 13:19:14 +0300

    genderClassification with DNN

--- genderClassificationWithDNN.py
+++ genderClassificationWithDNN.py
@@ -5,18 +5,18 @@ import dlib
 import cv2
 
 def shapePoints(shape):
-    coords = np.zeros((68, 2), dtype="int")
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-    return coords
+	coords = np.zeros((68, 2), dtype="int")
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+	return coords
 
 
 def rectPoints(rect):
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-    return (x, y, w, h)
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+	return (x, y, w, h)
 
 
 # pre-trained model
@@ -26,29 +26,52 @@ configFile = "faceDetection/models/dnn/deploy.prototxt"
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
 def detectFacesWithDNN(frame):
-    # A neural network that really supports the input value
-    size = (300,300)
+	# A neural network that really supports the input value
+	size = (300,300)
 
-    # After executing the average reduction, the image needs to be scaled
-    scalefactor = 1.0
+	# After executing the average reduction, the image needs to be scaled
+	scalefactor = 1.0
 
-    # These are our mean subtraction values. They can be a 3-tuple of the RGB means or
-    # they can be a single value in which case the supplied value is subtracted from every
-    # channel of the image.
-    swapRB = (104.0, 117.0, 123.0)
+	# These are our mean subtraction values. They can be a 3-tuple of the RGB means or
+	# they can be a single value in which case the supplied value is subtracted from every
+	# channel of the image.
+	swapRB = (104.0, 117.0, 123.0)
 
-    height, width = frame.shape[:2]
-    resizedFrame = cv2.resize(frame, size)
-    blob = cv2.dnn.blobFromImage(resizedFrame, scalefactor, size, swapRB)
-    net.setInput(blob)
-    dnnFaces = net.forward()
-    for i in range(dnnFaces.shape[2]):
-        confidence = dnnFaces[0, 0, i, 2]
-        if confidence > 0.5:
-            box = dnnFaces[0, 0, i, 3:7] * np.array([width-20, height+20, width-20, height+20])
-            (x, y, x1, y1) = box.astype("float32")
-            cv2.rectangle(frame, (x, y), (x1, y1), (193, 69, 42), 2)
-    return frame
+	height, width = frame.shape[:2]
+	resizedFrame = cv2.resize(frame, size)
+	blob = cv2.dnn.blobFromImage(resizedFrame, scalefactor, size, swapRB)
+	net.setInput(blob)
+	dnnFaces = net.forward()
+	for i in range(dnnFaces.shape[2]):
+		confidence = dnnFaces[0, 0, i, 2]
+		if confidence > 0.5:
+			box = dnnFaces[0, 0, i, 3:7] * np.array([width, height, width, height])
+			(x, y, x1, y1) = box.astype("int")
+			resized = frame[y-20: y1+30, x-10:x1+10]
+			cv2.imshow("resized", resized)
+			try:
+				frame_resize = cv2.resize(resized, genderTargetSize)
+			except:
+				continue
+
+			frame_resize = frame_resize.astype("float32")
+			frame_scaled = frame_resize/255.0
+			frame_reshape = np.reshape(frame_scaled,(1, 100, 100 ,3))
+			frame_vstack = np.vstack([frame_reshape])
+			gender_prediction = genderClassifier.predict(frame_vstack)
+			gender_probability = np.max(gender_prediction)
+			color = (255,255,255)
+			if(gender_probability > 0.6):
+
+				gender_label = np.argmax(gender_prediction)
+				gender_result = genders[gender_label]["label"]
+				color = genders[gender_label]["color"]
+				cv2.putText(frame, gender_result , (x+5, y1-5),
+				cv2.FONT_HERSHEY_SIMPLEX, 1 , color, 2,   cv2.LINE_AA)
+				cv2.rectangle(frame, (x, y), (x1, y1), color, 2)
+			else:
+				cv2.rectangle(frame, (x, y), (x1, y1), color, 2)
+	return frame
 
 
 genderModelPath = 'models\genderModel_VGG16.hdf5'
@@ -56,24 +79,23 @@ genderClassifier = load_model(genderModelPath, compile=False)
 genderTargetSize = genderClassifier.input_shape[1:3]
 
 genders =  {
-    0: { "label": "Female", "color": (245,215,130) },
-    1: { "label": "Male", "color": (148,181,192) },
+	0: { "label": "Female", "color": (245,215,130) },
+	1: { "label": "Male", "color": (148,181,192) },
 }
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
+	ret, frame = cap.read()
 
-    if not ret:
-        break
+	if not ret:
+		break
 
-    frame = detectFacesWithDNN(frame)
-
-    cv2.imshow("Gender Classification", frame)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-    	break
+	frame = detectFacesWithDNN(frame)
+	cv2.imshow("Gender Classification", frame)
+	k = cv2.waitKey(1) & 0xFF
+	if k == 27:
+		break
 
 
 cap.release()
