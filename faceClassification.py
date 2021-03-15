cf55c6bf20d876697d38b48c2ae1ae5b641eb5cc
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-03-15 23:32:34 +0300

    Add OOP feature but not finish yet

---
+++ faceClassification.py
@@ -0,0 +1,18 @@
+# Importing required packages
+import numpy as np
+import cv2
+from faceProcess import FaceProcess
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    faces = FaceProcess(frame)
+    faces = faces.run()
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
--- frames.py
+++
@@ -1,8 +0,0 @@
-# Importing required packages
-from keras.models import load_model
-import numpy as np
-import cv2
-
-class Frames:
-    def __init__(self, x, y):
-        self.x =
