1299ec2114b8a262e42a16a1fdd1bcc1ddb7c008
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-03-20 11:25:25 +0300

    add age prediction python file

---
+++ agePrediction.py
@@ -0,0 +1,75 @@
+# Importing required packages
+from keras.models import load_model
+import numpy as np
+import cv2
+
+genderModelPath = 'models/age_model_with_cnn.h5'
+genderClassifier = load_model(genderModelPath, compile=False)
+genderTargetSize = genderClassifier.input_shape[1:3]
+
+# pre-trained model
+modelFile = "faceDetection/models/dnn/res10_300x300_ssd_iter_140000.caffemodel"
+# prototxt has the information of where the training data is located.
+configFile = "faceDetection/models/dnn/deploy.prototxt"
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+
+
+def detectFacesWithDNN(frame):
+    # A neural network that really supports the input value
+    size = (300, 300)
+
+    # After executing the average reduction, the image needs to be scaled
+    scalefactor = 1.0
+
+    # These are our mean subtraction values. They can be a 3-tuple of the RGB means or
+    # they can be a single value in which case the supplied value is subtracted from every
+    # channel of the image.
+    swapRB = (104.0, 117.0, 123.0)
+
+    height, width = frame.shape[:2]
+    resizedFrame = cv2.resize(frame, size)
+    blob = cv2.dnn.blobFromImage(resizedFrame, scalefactor, size, swapRB)
+    net.setInput(blob)
+    dnnFaces = net.forward()
+    for i in range(dnnFaces.shape[2]):
+        confidence = dnnFaces[0, 0, i, 2]
+        if confidence > 0.5:
+            box = dnnFaces[0, 0, i, 3:7] * np.array(
+                [width, height, width, height])
+            (x, y, x1, y1) = box.astype("int")
+            resized = frame[y:y1, x:x1]
+            try:
+                frame_resize = cv2.resize(resized, genderTargetSize)
+            except:
+                continue
+
+            frame_resize = frame_resize.astype("float32")
+            frame_scaled = frame_resize / 255.0
+            frame_reshape = np.reshape(frame_scaled, (1, 32, 32, 3))
+            frame_vstack = np.vstack([frame_reshape])
+            gender_prediction = genderClassifier.predict(frame_vstack)
+            gender_probability = np.max(gender_prediction)
+            color = (255, 255, 255)
+            if (gender_probability > 0.4):
+                print("gender_probability", gender_probability)
+                print("gender_prediction", gender_prediction)
+    return frame
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    # frame = cv2.resize(frame, (720,420))
+
+    if not ret:
+        break
+
+    frame = detectFacesWithDNN(frame)
+    cv2.imshow("Gender Classification", frame)
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
