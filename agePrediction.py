8b18773eb7acd3dd4bfec4b6f0ae4dd547f22d8a
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-03-28 00:36:45 +0300

    age prediction

--- agePrediction.py
+++ agePrediction.py
@@ -1,72 +1,65 @@
 # Importing required packages
 from keras.models import load_model
 import numpy as np
+import dlib
 import cv2
 
-genderModelPath = 'models/age_model_with_smallerVGG.h5'
-genderClassifier = load_model(genderModelPath, compile=False)
-genderTargetSize = genderClassifier.input_shape[1:3]
 
-# pre-trained model
-modelFile = "faceDetection/models/dnn/res10_300x300_ssd_iter_140000.caffemodel"
-# prototxt has the information of where the training data is located.
-configFile = "faceDetection/models/dnn/deploy.prototxt"
-net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+def shapePoints(shape):
+    coords = np.zeros((68, 2), dtype="int")
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
 
 
-def detectFacesWithDNN(frame):
-    # A neural network that really supports the input value
-    size = (300, 300)
+def rectPoints(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
 
-    # After executing the average reduction, the image needs to be scaled
-    scalefactor = 1.0
 
-    # These are our mean subtraction values. They can be a 3-tuple of the RGB means or
-    # they can be a single value in which case the supplied value is subtracted from every
-    # channel of the image.
-    swapRB = (104.0, 117.0, 123.0)
+faceLandmarks = "faceDetection/models/dlib/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(faceLandmarks)
 
-    height, width = frame.shape[:2]
-    resizedFrame = cv2.resize(frame, size)
-    blob = cv2.dnn.blobFromImage(resizedFrame, scalefactor, size, swapRB)
-    net.setInput(blob)
-    dnnFaces = net.forward()
-    for i in range(dnnFaces.shape[2]):
-        confidence = dnnFaces[0, 0, i, 2]
-        if confidence > 0.5:
-            box = dnnFaces[0, 0, i, 3:7] * np.array(
-                [width, height, width, height])
-            (x, y, x1, y1) = box.astype("int")
-            resized = frame[y:y1, x:x1]
-            try:
-                frame_resize = cv2.resize(resized, genderTargetSize)
-            except:
-                continue
-
-            frame_resize = frame_resize.astype("float32")
-            frame_scaled = frame_resize / 255.0
-            frame_reshape = np.reshape(frame_scaled, (1, 100, 100, 3))
-            frame_vstack = np.vstack([frame_reshape])
-            gender_prediction = genderClassifier.predict(frame_vstack)
-            gender_probability = np.max(gender_prediction)
-            color = (255, 255, 255)
-            if (gender_probability > 0.4):
-                print("gender_prediction", np.argmax(gender_prediction))
-    return frame
-
+emotionModelPath = 'models/age_prediction_with_cnn.h5'  # fer2013_mini_XCEPTION.110-0.65
+emotionClassifier = load_model(emotionModelPath, compile=False)
+emotionTargetSize = emotionClassifier.input_shape[1:3]
 
 cap = cv2.VideoCapture(0)
-
 while True:
     ret, frame = cap.read()
-    # frame = cv2.resize(frame, (720,420))
+    frame = cv2.resize(frame, (720, 480))
 
     if not ret:
         break
 
-    frame = detectFacesWithDNN(frame)
-    cv2.imshow("Gender Classification", frame)
-    k = cv2.waitKey(10) & 0xFF
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(grayFrame, 0)
+    for rect in rects:
+        shape = predictor(grayFrame, rect)
+        points = shapePoints(shape)
+        (x, y, w, h) = rectPoints(rect)
+        grayFace = grayFrame[y:y + h, x:x + w]
+        try:
+            grayFace = cv2.resize(grayFace, (48, 48))
+        except:
+            continue
+
+        grayFace = grayFace.reshape(-1, 48, 48, 1)
+        emotion_prediction = emotionClassifier.predict(grayFace)
+        emotion_probability = np.max(emotion_prediction)
+        if (emotion_probability > 0.45):
+            emotion_label_arg = np.argmax(emotion_prediction)
+            color = (100, 100, 100)
+            cv2.putText(frame, str(emotion_label_arg), (x + 25, y + h + 36),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1,
+                        cv2.LINE_AA)
+
+    cv2.imshow("Emotion Recognition", frame)
+    k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
 
